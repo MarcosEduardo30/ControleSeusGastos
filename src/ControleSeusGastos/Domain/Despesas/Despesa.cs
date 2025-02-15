510e186068b204f763d0d855a587ccecb253868d
MarcosEduardo30 <marcos302k02@gmail.com>
2025-02-15 19:28:34 -0300

    feat: Adicionado as entidades de despesa e categoria

---
+++ src/ControleSeusGastos/Domain/Despesas/Despesa.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Categorias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Despesas
+{
+    public class Despesa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }    
+        public Categoria Categoria { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
