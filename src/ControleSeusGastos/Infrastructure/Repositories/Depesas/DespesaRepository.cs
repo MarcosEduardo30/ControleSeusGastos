a02292774abf4c95bfe9232fe60f7cfb8cd9ae5a
MarcosEduardo30 <marcos302k02@gmail.com>
2025-02-20 17:31:34 -0300

    feat: adicionado o repositorio para as despesas

---
+++ src/ControleSeusGastos/Infrastructure/Repositories/Depesas/DespesaRepository.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Despesas;
+using Infrastructure.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Depesas
+{
+    public class DespesaRepository : IDespesaRepository
+    {
+        internal readonly AppDbContext _dbContext;
+        public DespesaRepository(AppDbContext dbContext) { 
+            _dbContext = dbContext;
+        }
+        public async Task<int> criar(Despesa despesa)
+        {
+            _dbContext.Add(despesa);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Despesa?> buscaPorId(int id)
+        {
+            return await _dbContext.Despesas.FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task<int> atualizar(Despesa despesa)
+        {
+            _dbContext.Despesas.Update(despesa);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> removerPorId(int id)
+        {
+            return await _dbContext.Despesas.Where(d => d.Id == id).ExecuteDeleteAsync();   
+        }
+    }
+}
---
+++ src/ControleSeusGastos/Infrastructure/Repositories/Depesas/IDespesaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Despesas;
+
+namespace Infrastructure.Repositories.Depesas
+{
+    public interface IDespesaRepository
+    {
+        public Task<int> criar(Despesa despesa);
+        public Task<Despesa?> buscaPorId(int id);
+        //public Task<List<Despesa?>> buscarPorIdUsuario(int idUsuario);
+        public Task<int> atualizar(Despesa despesa);
+        public Task<int> removerPorId(int id);
+
+    }
+}
