6bd667f052c1a6ce179be5f54db01409969efd22
MarcosEduardo30 <marcos302k02@gmail.com>
2025-02-24 15:19:58 -0300

    feat: Adicionado a lógica de négocio para editar despesas

---
+++ src/ControleSeusGastos/Application/Despesas/EditarDespesa/EditarDespesa.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Repositories.Depesas;
+
+namespace Application.Despesas.EditarDespesa
+{
+    public class EditarDespesa : IEditarDespesa
+    {
+        private readonly IDespesaRepository _despesaRepository;
+
+        public EditarDespesa(IDespesaRepository despesaRepository) { 
+            _despesaRepository = despesaRepository;
+        }
+        public async Task<EditarDespesaOutput?> Editar(EditarDespesaInput NovaDespesa)
+        {
+            var despesaAntiga = await _despesaRepository.buscaPorId(NovaDespesa.Id);
+            if (despesaAntiga == null) {
+                return null;
+            }
+
+            despesaAntiga.Nome = NovaDespesa.Nome;
+            despesaAntiga.Descricao = NovaDespesa.Descricao;
+            despesaAntiga.Data = NovaDespesa.Data;
+
+            await _despesaRepository.atualizar(despesaAntiga);
+
+            var resul = new EditarDespesaOutput
+            {
+                Nome = despesaAntiga.Nome,
+                Descricao = despesaAntiga.Descricao,
+                Data = despesaAntiga.Data,
+            };
+
+            return resul;
+        }
+    }
+}
---
+++ src/ControleSeusGastos/Application/Despesas/EditarDespesa/IEditarDespesa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Despesas.EditarDespesa
+{
+    public interface IEditarDespesa
+    {
+        public Task<EditarDespesaOutput?> Editar(EditarDespesaInput NovaDespesa);
+    }
+}
