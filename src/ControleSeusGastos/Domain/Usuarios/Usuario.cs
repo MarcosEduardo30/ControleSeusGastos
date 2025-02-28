d2476c39704884d73fe9f1947b62988c38c96218
MarcosEduardo30 <marcos302k02@gmail.com>
2025-02-28 14:30:58 -0300

    feat: adicionado a entidade de usuario

---
+++ src/ControleSeusGastos/Domain/Usuarios/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Usuarios
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string username {  get; set; }
+        public string password { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
