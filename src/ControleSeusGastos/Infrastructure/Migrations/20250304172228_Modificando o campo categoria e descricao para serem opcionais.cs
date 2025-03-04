e349eb23d265c6969d2681eb899a248c08c3186a
MarcosEduardo30 <marcos302k02@gmail.com>
2025-03-04 20:30:14 -0300

    feat: Criacao das migrations para a mudança na entidade de despesas

---
+++ src/ControleSeusGastos/Infrastructure/Migrations/20250304172228_Modificando o campo categoria e descricao para serem opcionais.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modificandoocampocategoriaedescricaoparaseremopcionais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Despesas_Categorias_Categoria_Id",
+                table: "Despesas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Despesas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Categoria_Id",
+                table: "Despesas",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Despesas_Categorias_Categoria_Id",
+                table: "Despesas",
+                column: "Categoria_Id",
+                principalTable: "Categorias",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Despesas_Categorias_Categoria_Id",
+                table: "Despesas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Despesas",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Categoria_Id",
+                table: "Despesas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Despesas_Categorias_Categoria_Id",
+                table: "Despesas",
+                column: "Categoria_Id",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
