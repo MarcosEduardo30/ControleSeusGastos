5d9d36a96ac9cf52e4c799632f53e5331e7e8180
MarcosEduardo30 <marcos302k02@gmail.com>
2025-03-09 17:52:28 -0300

    test: Adicionado os testes de integração de buscar despesa

---
+++ src/ControleSeusGastos/ControleSeusGastos.Integration.Tests/BuscarDespesaTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.Despesas.BuscarDespesa.DTO;
+using ControleSeusGastos.API;
+using ControleSeusGastos.API.Resultados;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace ControleSeusGastos.Integration.Tests
+{
+    public class BuscarDespesaTests: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public BuscarDespesaTests(WebApplicationFactory<Program> factory) { 
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task DeveriaRetornarDespesa_QueIdValido_AoBuscarDespesa()
+        {
+            //Arrange
+            int id = 10;
+
+            //Act
+            var response = await _client.GetAsync($"/Despesas/{id}");
+            var result = await response.Content.ReadFromJsonAsync<ResultadoAPI<BuscarDespesaOutput>>();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Null(result.erros);
+            Assert.NotNull(result.data);
+            Assert.IsType<BuscarDespesaOutput>(result.data);
+        }
+
+
+        [Fact]
+        public async Task DeveriaRetornarErro_QueIdInvalido_AoBuscarDespesa()
+        {
+            //Arrange
+            int id = 0;
+
+            //Act
+            var response = await _client.GetAsync($"/Despesas/{id}");
+            var result = await response.Content.ReadFromJsonAsync<ResultadoAPI<BuscarDespesaOutput>>();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.False(response.IsSuccessStatusCode);
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+            Assert.Null(result.data);
+        }
+    }
+}
---
+++ src/ControleSeusGastos/ControleSeusGastos.Integration.Tests/ExcluirDespesaTests.cs
@@ -0,0 +1,20 @@
+﻿using ControleSeusGastos.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ControleSeusGastos.Integration.Tests
+{
+    public class ExcluirDespesaTests: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _httpClient;
+
+        public ExcluirDespesaTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+
+
+
+    }
+}
