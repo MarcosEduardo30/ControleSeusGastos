205190d1db271aec4c17c51576d33aac1c69d8ce
MarcosEduardo30 <marcos302k02@gmail.com>
2025-03-09 19:12:37 -0300

    test: Adicionado os testes de editar despesa

--- src/ControleSeusGastos/ControleSeusGastos.Integration.Tests/EditarDespesaTests.cs
+++ src/ControleSeusGastos/ControleSeusGastos.Integration.Tests/EditarDespesaTests.cs
@@ -1,9 +1,14 @@
-﻿using ControleSeusGastos.API;
+﻿using Application.Services.Despesas.CriarDespesa.DTO;
+using Application.Services.Despesas.EditarDespesa.DTO;
+using Application.Validacao;
+using ControleSeusGastos.API;
+using ControleSeusGastos.API.Resultados;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
 
 namespace ControleSeusGastos.Integration.Tests
 {
-    internal class EditarDespesaTests
+    public class EditarDespesaTests: IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
         private readonly HttpClient _httpClient;
@@ -13,5 +18,65 @@ namespace ControleSeusGastos.Integration.Tests
             _factory = factory;
             _httpClient = _factory.CreateClient();
         }
+
+
+        [Fact]
+        public async Task DeveriaRetonarDespesa_QueDadosValidos_AoEditarDespesa()
+        {
+            //arrange
+            var input = new EditarDespesaInput
+            {
+                Valor = 100,
+                Nome = "Teste Atualizado",
+                Descricao = "Descrição Atualizada",
+                Categoria_id = 1,
+                Data = new DateTime(2025, 01, 01).ToUniversalTime()
+            };
+
+            int id = 10;
+
+            //act
+            var response = await _httpClient.PutAsJsonAsync($"/Despesas/{id}", input);
+            var result = await response.Content.ReadFromJsonAsync<ResultadoAPI<EditarDespesaOutput>>();
+
+            //assert
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Null(result.erros);
+            Assert.NotNull(result.data);
+            Assert.Equal(100, result.data.Valor);
+            Assert.Equal("Teste", result.data.Nome);
+            Assert.Equal("Descrição teste", result.data.Descricao);
+        }
+
+        [Fact]
+        public async Task DeveriaRetornarErro_QueIdDespesaInvalido_AoEditarDespesa()
+        {
+            //arrange
+            var input = new EditarDespesaInput
+            {
+                Valor = 100,
+                Nome = "Teste Atualizado",
+                Descricao = "Descrição Atualizada",
+                Categoria_id = 1,
+                Data = new DateTime(2025, 01, 01).ToUniversalTime()
+            };
+
+            int id = 0;
+
+            //act
+            var response = await _httpClient.PostAsJsonAsync("/Despesas", input);
+            var result = await response.Content.ReadFromJsonAsync<ResultadoAPI<EditarDespesaOutput>>();
+
+            //assert
+            Assert.False(response.IsSuccessStatusCode);
+            Assert.Null(result.data);
+            Assert.NotNull(result.erros);
+            Assert.NotEmpty(result.erros);
+            Assert.Single(result.erros);
+            Assert.IsType<List<Erro>>(result.erros);
+            Assert.Equal("Despesa_Invalida", result.erros[0].nome);
+        }
+
+
     }
 }
