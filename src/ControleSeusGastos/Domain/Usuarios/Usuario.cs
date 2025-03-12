13ceee23fef8f6956205ab65003178a35a4ea18d
MarcosEduardo30 <marcos302k02@gmail.com>
2025-03-12 16:01:20 -0300

    feat: Adicionado os campos nome e email ao usuario

--- src/ControleSeusGastos/Domain/Usuarios/Usuario.cs
+++ src/ControleSeusGastos/Domain/Usuarios/Usuario.cs
@@ -3,6 +3,8 @@
     public class Usuario
     {
         public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
         public string username {  get; set; }
         public string password { get; set; }
         public DateTime created_at { get; set; }
---
+++ src/ControleSeusGastos/Infrastructure/Migrations/20250312190015_Adicionado Nome e Email ao usuario.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionadoNomeeEmailaousuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Usuarios");
+        }
+    }
+}
